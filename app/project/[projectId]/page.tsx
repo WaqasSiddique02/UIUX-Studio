356cca131e3c001e02a2456892ed54119436e639
Muhammad Waqas Siddique <siddique02mwaqas@gmail.com>
2026-01-29 19:53:20 +0500

    Generate Screen Mockup UIUX

--- app/project/[projectId]/page.tsx
+++ app/project/[projectId]/page.tsx
@@ -35,11 +35,12 @@ function ProjectCanvasPlayGround() {
   useEffect(() => {
     if (projectDetail && screenConfig && screenConfig?.length == 0) {
       generateScreenConfig();
+    } else if (projectDetail && screenConfig) {
+      GenerateScreenUIUX();
     }
   }, [projectDetail && screenConfig]);
 
   const generateScreenConfig = async () => {
-    console.log("Generate Screen Config as none exists");
     setLoading(true);
     setLoadingMsg("Generating Screen Config...");
     const result = await axios.post("/api/generate-config", {
@@ -49,6 +50,27 @@ function ProjectCanvasPlayGround() {
     });
     console.log(result.data);
     GetProjectDetail();
+    setLoading(false);
+  };
+
+  const GenerateScreenUIUX = async () => {
+    setLoading(true);
+    for (let index = 0; index < screenConfig?.length; index++) {
+      const screen = screenConfig[index];
+      if (screen?.code) continue;
+
+      setLoadingMsg("Generating Screen" + index + 1);
+      const result = await axios.post("/api/generate-screen-ui", {
+        projectId,
+        screenId: screen?.screenId,
+        screenName: screen?.screenName,
+        purpose: screen?.purpose,
+        screenDescription: screen?.screenDescription,
+      });
+
+      console.log(result.data);
+      setScreenConfig(prev=>prev.map((item,i)=>(i===index?result.data:item)))
+    }
     setLoading(false);
   };
 
